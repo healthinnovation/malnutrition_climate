5340c50387412ef0b4afeaf1553171d07d91649b
ambarja <antony.barja8@gmail.com>
2022-12-22 10:11:02 -0500

    up

--- 01_get_information.R
+++ 01_get_information.R
@@ -18,6 +18,41 @@ pp <- ee$ImageCollection$Dataset$IDAHO_EPSCOR_TERRACLIMATE %>%
   ee$ImageCollection$toBands() %>% 
   ee$Image$clip(region_ee)
 
+temp_max <- ee$ImageCollection$Dataset$IDAHO_EPSCOR_TERRACLIMATE %>% 
+  ee$ImageCollection$filterDate("2019-01-01","2019-12-31") %>% 
+  ee$ImageCollection$select("tmmx") %>% 
+  ee$ImageCollection$toBands() %>% 
+  ee$Image$clip(region_ee)
+
+ndvi <- ee$ImageCollection$Dataset$MODIS_006_MOD13Q1 %>% 
+  ee$ImageCollection$filter(ee$Filter$calendarRange(2019,2019,"month")) %>%
+  ee$ImageCollection$select("NDVI") %>% 
+  ee$ImageCollection$toBands() %>% 
+  ee$Image$clip(region_ee) %>% 
+  ee$Image$multiply(0.0001)
+
+# 3. Downloading raster layer ---------------------------------------------
+
+ee_as_raster(
+  image = pp,
+  region = region_ee,
+  dsn = "rasters/pp_2019.tif",
+  scale = 1000
+  )
+
+ee_as_raster(
+  image = temp_max,
+  region = region_ee,
+  dsn = "rasters/temp_max_2019.tif",
+  scale = 1000
+)
+
+ee_as_raster(
+  image = ndvi,
+  region = region_ee,
+  dsn = "rasters/ndvi_2019.tif",
+  scale = 1000
+)
 
 
 
